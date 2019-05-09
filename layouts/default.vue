7d822237f002953006156a67552a40b7b6a739da
liutao <vaceliu@gmail.com>
2019-05-09 18:03:52 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,229 @@
+<template>
+  <div>
+    <a-back-top />
+    <a-layout id="layout-top-side" style="margin: 0 50px">
+      <a-layout-header
+        class="header"
+        :style="{ background: '#fff', height: '433px', padding: '0' }"
+      >
+        <div class="logo">
+          <img src="../static/image/logo.jpg" />
+        </div>
+        <a-menu
+          mode="horizontal"
+          :default-selected-keys="['1']"
+          :style="{
+            backgroundColor: 'rgb(28, 97, 162)',
+            color: '#fff'
+          }"
+        >
+          <a-menu-item key="1">首页</a-menu-item>
+          <a-menu-item key="2">平台简介</a-menu-item>
+          <a-menu-item key="3">采购公告</a-menu-item>
+          <a-menu-item key="4">中标公告</a-menu-item>
+          <a-menu-item key="5">服务指南</a-menu-item>
+          <a-menu-item key="6">新闻动态</a-menu-item>
+        </a-menu>
+        <a-carousel arrows :autoplay="true">
+          <div
+            slot="prevArrow"
+            class="custom-slick-arrow"
+            style="left: 10px;zIndex: 1"
+          >
+            <a-icon type="left-circle" />
+          </div>
+          <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px">
+            <a-icon type="right-circle" />
+          </div>
+          <div>
+            <img src="../static/image/banner1.jpg" width="100%" />
+          </div>
+          <div>
+            <img src="../static/image/banner2.jpg" width="100%" />
+          </div>
+          <div>
+            <img src="../static/image/banner3.jpg" width="100%" />
+          </div>
+          <div>
+            <img src="../static/image/banner4.jpg" width="100%" />
+          </div>
+        </a-carousel>
+      </a-layout-header>
+      <a-layout>
+        <a-layout-sider width="250" style="background:#fff">
+          <div
+            :style="{
+              padding: '10px',
+              background: 'rgb(28, 97, 162)',
+              color: '#fff'
+            }"
+          >
+            <span>按采购组织查看项目</span>
+          </div>
+          <a-menu
+            mode="inline"
+            :default-selected-keys="['1']"
+            :style="{ height: '100%', borderRight: 0 }"
+          >
+            <a-menu-item key="1">久泰集团有限公司</a-menu-item>
+            <a-menu-item key="2">久泰能源科技有限公司</a-menu-item>
+            <a-menu-item key="3">久泰能源内蒙古有限公司</a-menu-item>
+            <a-menu-item key="4">久泰能源(准格尔)有限公司</a-menu-item>
+            <a-menu-item key="5">久泰能源(广州)有限公司</a-menu-item>
+            <a-menu-item key="6">久泰能源(张家港)有限公司</a-menu-item>
+            <a-menu-item key="7">久泰能源(北京)有限公司</a-menu-item>
+            <a-menu-item key="8">山东联建华星精瓷有限公司</a-menu-item>
+            <a-menu-item key="9">山东兴泰机械装备工程有限责任公司</a-menu-item>
+            <a-menu-item key="10">内蒙古久泰新材料有限公司</a-menu-item>
+            <a-menu-item key="11">鄂尔多斯市久泰建筑安装有限公司</a-menu-item>
+            <a-menu-item key="12">久泰集团销售有限责任公司</a-menu-item>
+            <a-menu-item key="13">内蒙古久泰新材料销售有限公司</a-menu-item>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout style="padding: 0 15px 15px">
+          <a-layout-content>
+            <nuxt />
+          </a-layout-content>
+          <a-layout-footer style="text-align: center">
+            Copyright © 2019 Savor,All Rights Reserved.
+          </a-layout-footer>
+        </a-layout>
+        <div :style="{ background: '#fff' }">
+          <div
+            :style="{
+              padding: '10px',
+              background: 'rgb(28, 97, 162)',
+              color: '#fff'
+            }"
+          >
+            <span>供应商登录</span>
+          </div>
+          <a-form
+            :style="{
+              background: '#fff',
+              textAlgin: 'center',
+              marginLeft: '20px',
+              marginTop: '10px'
+            }"
+          >
+            <a-form-item label="用户名" style="margin-bottom: 10px">
+              <a-input
+                v-decorator="[
+                  'username',
+                  {
+                    rules: [{ required: true, message: '请输入用户名!' }]
+                  }
+                ]"
+              />
+            </a-form-item>
+            <a-form-item label="密码" style="margin-bottom: 10px">
+              <a-input
+                v-decorator="[
+                  'password',
+                  {
+                    rules: [{ required: true, message: '请输入密码!' }]
+                  }
+                ]"
+              />
+            </a-form-item>
+            <a-form-item
+              :wrapper-col="{ span: 21, offset: 3 }"
+              :style="{ marginTop: '20px' }"
+            >
+              <a-button
+                type="primary"
+                :style="{ background: 'rgb(28, 97, 162)', width: '150px' }"
+                html-type="submit"
+              >
+                登录
+              </a-button>
+              <div :style="{ marginLeft: '5px' }">
+                <a>用户注册</a>
+                <a-divider type="vertical"></a-divider>
+                <a>忘记密码?</a>
+              </div>
+            </a-form-item>
+          </a-form>
+        </div>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.logo {
+  background-image: url('../static/image/head_bg.jpg');
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+.ant-carousel /deep/ .slick-slide {
+  text-align: center;
+  height: 253px;
+  line-height: 160px;
+  overflow: hidden;
+}
+
+.ant-carousel /deep/ .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #fff;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+}
+.ant-carousel /deep/ .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel /deep/ .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+.ant-carousel >>> .slick-slide img {
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,343 @@
+<template>
+  <section class="container">
+    <div>
+      <a-form>
+        <a-form-item
+          label="综合信息查询"
+          :label-col="{ span: 6 }"
+          :wrapper-col="{ span: 12 }"
+        >
+          <a-input-search enter-button style="width:250px" @search="onSearch" />
+        </a-form-item>
+      </a-form>
+    </div>
+    <div style="margin-left: 15px">
+      <div
+        :style="{
+          color: 'rgb(28, 97, 162)',
+          paddingBottom: '10px'
+        }"
+      >
+        <span
+          :style="{
+            float: 'left',
+            fontSize: '18px'
+          }"
+          >招标公告(851)
+        </span>
+        <a :style="{ float: 'right', marginRight: '10px' }">更多</a>
+      </div>
+      <a-divider />
+      <a-list item-layout="horizontal" :split="false">
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-08</span>
+          <a-list-item-meta description="050801招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-06</span>
+          <a-list-item-meta description="0506招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-05</span>
+          <a-list-item-meta description="0505招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-24</span>
+          <a-list-item-meta description="042401招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-18</span>
+          <a-list-item-meta description="0418招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-08</span>
+          <a-list-item-meta description="040801招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-03-08</span>
+          <a-list-item-meta description="030801招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-03-28</span>
+          <a-list-item-meta description="032801招投标">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+      </a-list>
+    </div>
+    <div :style="{ marginTop: '20px', marginLeft: '15px' }">
+      <div
+        :style="{
+          color: 'rgb(28, 97, 162)',
+          paddingBottom: '10px'
+        }"
+      >
+        <span
+          :style="{
+            float: 'left',
+            fontSize: '18px'
+          }"
+          >询价公告(238)
+        </span>
+        <a :style="{ float: 'right', marginRight: '10px' }">更多</a>
+      </div>
+      <a-divider />
+      <a-list item-layout="horizontal" :split="false">
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-24</span>
+          <a-list-item-meta description="张家港油漆询价询比价公告">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-24</span>
+          <a-list-item-meta description="张家港公司空压机配件采购询比价公告">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-04-28</span>
+          <a-list-item-meta description="张家港公司制氮机组配件采购询比价公告">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-08</span>
+          <a-list-item-meta description="久泰内蒙古公司五金杂品采购询比价公告">
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-08</span>
+          <a-list-item-meta
+            description="工程部高强螺栓（请购单1463）询比价公告"
+          >
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-08</span>
+          <a-list-item-meta
+            description="内蒙古公司药剂类（5月三氯化铁）询比价公告"
+          >
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+        <a-list-item>
+          <span slot="actions" style="margin-right:20px">2019-05-08</span>
+          <a-list-item-meta
+            description="内蒙古公司阀门类（气动钨钢旋转阀）询比价公告"
+          >
+            <a-avatar
+              slot="avatar"
+              :style="{
+                marginTop: '-5px',
+                marginRight: '-15px',
+                background: '#fff',
+                color: 'lightgray'
+              }"
+            >
+              <a-icon type="caret-right" theme="filled" />
+            </a-avatar>
+          </a-list-item-meta>
+        </a-list-item>
+      </a-list>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSearch() {}
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  /* min-height: 100vh; */
+  /* display: flex; */
+  justify-content: center;
+  /* align-items: center; */
+  /* text-align: center; */
+  background: #fff;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
